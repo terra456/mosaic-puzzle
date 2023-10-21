3d54036c13db9285b17355b0a3de39ffd24dab40
terra456 <terra.456@yandex.ru>
2023-10-21 12:10:43 +0500

    feat: add webpack and eslint

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+export const printWidth = 100;
+export const singleQuote = true;
+export const trailingComma = 'all';
+export const bracketSpacing = true;
+export const jsxBracketSameLine = false;
+export const tabWidth = 2;
+export const semi = true;
